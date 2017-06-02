7d37b851889126bfa8540ff67cf69e6a7d7c897b
Amira Maythe <amiramaythe16@gmail.com>
2017-06-02 18:14:13 -0300

    Iniciando projeto

---
+++ CircleFramework/CircleFramework/CircleFramework.h
@@ -0,0 +1,19 @@
+//
+//  CircleFramework.h
+//  CircleFramework
+//
+//  Created by Amira Maythe Vasquez on 2/6/17.
+//  Copyright © 2017 Amira Maythe Vasquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleFramework.
+FOUNDATION_EXPORT double CircleFrameworkVersionNumber;
+
+//! Project version string for CircleFramework.
+FOUNDATION_EXPORT const unsigned char CircleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleFramework/PublicHeader.h>
+
+
